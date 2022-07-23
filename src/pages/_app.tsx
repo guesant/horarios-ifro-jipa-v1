ed8447eb305f879d976fd8d54f426409c5701ccf
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2022-07-22 21:18:35 -0400

    feat: App: queryClient as component state

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import type { AppProps } from "next/app";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryClient,
+  QueryClientConfig,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { useState } from "react";
 
-const queryClient = new QueryClient({
+const queryClientConfig: QueryClientConfig = {
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
-});
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient(queryClientConfig));
+
   return (
     <>
       <QueryClientProvider client={queryClient}>
